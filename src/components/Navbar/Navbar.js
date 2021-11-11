79bcc2355b44889b9ae62c82cc87916b75b9a730
Rosada-eng <guilhermer4@al.insper.edu.br>
2021-11-11 13:18:42 -0300

    Adiciona menu do usuario

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,25 +1,27 @@
 import React from 'react';
 import styled from './Navbar.module.css';
 import { ReactComponent as Logo } from './dollar.svg';
-// import { ReactComponent as Wallet } from './wallet.svg';
 import { ReactComponent as Coin } from './dollar-coin.svg';
 import { ReactComponent as User } from '../../assets/account.svg';
 import { Link } from 'react-router-dom';
 import { UserContext } from '../../UserContext';
+import UserMenu from './UserMenu.js';
 
 const Navbar = () => {
   const global = React.useContext(UserContext);
+  const [menu, setMenu] = React.useState(false);
   return (
     <div className={styled.container}>
       <div className={styled.header}>
-        <Link className={styled.brand} to="/">
+        <Link to="/" className={styled.brand}>
           <Logo fill={'#ecb242c2'} />
           <p className={styled.title}>Crypto Simulator</p>
         </Link>
         {global.login && global.data ? (
-          <div className={styled.login}>
+          <div className={styled.login} onClick={() => setMenu(!menu)}>
             <p className={styled.logged}>{global.data.first_name}</p>
             <User className={styled.logged} fill={'#ecb242'} />
+            {menu ? <UserMenu /> : null}
           </div>
         ) : (
           <div className={styled.login}>
---
+++ src/components/Navbar/UserMenu.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { ReactComponent as Wallet } from './wallet.svg';
+import { ReactComponent as Logout } from '../../assets/logout.svg';
+import styled from './UserMenu.module.css';
+import { UserContext } from '../../UserContext';
+
+const UserMenu = () => {
+  const { userLogout } = React.useContext(UserContext);
+  return (
+    <div className={styled.menuBox}>
+      <div className={styled.container}>
+        <Link to="/" className={styled.link}>
+          <Wallet className={styled.logo} fill={'#eec643'} /> Carteira
+        </Link>
+        <hr className={styled.bar} />
+
+        <Link to="/login" className={styled.link} onClick={userLogout}>
+          <Logout className={styled.logo} fill={'#eec643'} />
+          Sair
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default UserMenu;
