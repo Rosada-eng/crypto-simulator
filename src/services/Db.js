ad392db6beea9d14ac5b87ca27ed281b9d800a57
Rosada-eng <guilhermer4@al.insper.edu.br>
2021-12-01 19:47:18 -0300

    altera link da URL do db para localhost

--- src/services/Db.js
+++ src/services/Db.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const db = axios.create({
-  baseURL: 'https://backend-cryptosimulator.herokuapp.com/',
+  baseURL: 'http://localhost:8000/',
   headers: {
     'Content-Type': 'application/json',
   },
